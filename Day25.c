caaa363635766ab10317c231e08e2be498f775f2
Utkarsh Sandeep Singh <500053648@stu.upes.ac.in>
2019-05-13 12:55:21 +0530

    Add files via upload

---
+++ Day25.c
@@ -0,0 +1,33 @@
+/*
+.Write a program to obtain a number N and increment its value by 1 if the number is divisible by 4 otherwise decrement its value by 1.
+
+Input:
+First line will contain a number N.
+Output:
+Output a single line, the new value of the number.
+
+Constraints
+0=N=1000
+Sample Input:
+5
+Sample Output:
+4
+EXPLANATION:
+Since 5 is not divisible by 4 hence, its value is decreased by 1.
+*/
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+	int x;
+	scanf("%d",&x);
+	if(x%4==0)
+	{
+		x++;
+	}
+	else
+	{
+		x--;
+	}
+	printf("%d\n",x);
+}
