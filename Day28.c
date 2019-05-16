8d36c1dc3ce5d65dda7b7a2cc948dac5529c66f1
Utkarsh Sandeep Singh <500053648@stu.upes.ac.in>
2019-05-16 21:53:38 +0530

    Add files via upload

---
+++ Day28.c
@@ -0,0 +1,34 @@
+/*
+All submissions for this problem are available.Write a program to take two numbers as input and print their difference if the first number is greater than the second number otherwise print their sum.
+
+Input:
+First line will contain the first number (N1)
+Second line will contain the second number (N2)
+Output:
+Output a single line containing the difference of 2 numbers (N1-N2) if the first number is greater than the second number otherwise output their sum (N1+N2).
+
+Constraints
+-1000=N1=1000
+-1000=N2=1000
+Sample Input:
+82
+28
+Sample Output:
+54
+*/
+#include <stdio.h>
+int main(void) {
+	// your code goes here
+	int x,y;
+	scanf("%d",&x);
+	scanf("%d",&y);
+	int z;
+	if(x>y)
+	z=x-y;
+	else
+	z=x+y;
+	printf("%d\n",z);
+	return 0;
+}
+
+
