a86c282da1edadcea652929a0f915dab7f3e4c9d
Utkarsh Sandeep Singh <500053648@stu.upes.ac.in>
2019-05-25 11:39:00 +0530

    Update Day36.c

--- Day36.c
+++ Day36.c
@@ -1,3 +1,20 @@
+/*
+All submissions for this problem are available.Write a program to take a character (C) as input and check whether the given character is a vowel or a consonant.
+
+NOTE:− Vowels are 'A', 'E', 'I', 'O', 'U'. Rest all alphabets are called consonants.
+
+Input:
+First line will contain the character C.
+Output:
+Print "Vowel" if the given character is a vowel, otherwise print "Consonant".
+
+Constraints
+C will be an upper case English alphabet
+Sample Input:
+Z
+Sample Output:
+Consonant
+*/
 #include <stdio.h>
 
 int main(void) {
