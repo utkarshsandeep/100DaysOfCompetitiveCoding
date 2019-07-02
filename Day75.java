33699db9dea546ab9ad0ba8cd3a36c3493dc9608
Utkarsh Sandeep Singh <500053648@stu.upes.ac.in>
2019-07-02 23:58:18 +0530

    Add files via upload

---
+++ Day75.java
@@ -0,0 +1,60 @@
+/*
+Kristen loves playing with and comparing numbers. She thinks that if she takes two different positive numbers, the one whose digits sum to a larger number is better than the other. If the sum of digits is equal for both numbers, then she thinks the smaller number is better. For example, Kristen thinks that  is better than  and that  is better than .
+
+Given an integer, , can you find the divisor of  that Kristin will consider to be the best?
+
+Input Format
+
+A single integer denoting .
+
+Constraints
+
+Output Format
+
+Print an integer denoting the best divisor of .
+
+Sample Input 0
+
+12
+Sample Output 0
+
+6
+Explanation 0
+
+The set of divisors of  can be expressed as . The divisor whose digits sum to the largest number is  (which, having only one digit, sums to itself). Thus, we print  as our answer.
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int i,p=0;
+        boolean f=false;
+        for(i=2;i<n;i++)
+        {
+            if(n%i==0)
+            {
+                f=true;
+                p=i;
+            }
+        }
+        if(f)
+        System.out.println(p);
+        else
+        System.out.println(n);
+        scanner.close();
+    }
+}
+
