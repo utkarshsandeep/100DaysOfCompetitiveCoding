b0fcdb17515b5ea210c62c9f503e471fea53ccd5
Utkarsh Sandeep Singh <500053648@stu.upes.ac.in>
2019-07-03 17:09:00 +0530

    Add files via upload

---
+++ Day76.c
@@ -0,0 +1,106 @@
+/*
+Jim is off to a party and is searching for a matching pair of socks. His drawer is filled with socks, each pair of a different color. In its worst case scenario, how many socks (x) should Jim remove from his drawer until he finds a matching pair?
+
+Input Format 
+The first line contains the number of test cases T. 
+Next T lines contains an integer N which indicates the total pairs of socks present in the drawer.
+
+Output Format 
+Print the number of Draws (x) Jim makes in the worst case scenario.
+
+Constraints 
+ 
+
+Sample Input
+
+2
+1
+2
+Sample Output
+
+2
+3
+Explanation 
+Case 1 : A pair of socks are present, hence exactly 2 draws for the socks to match. 
+Case 2 : 2 pair of socks are present in the drawer. The first and the second draw might result in 2 socks of different color. The 3rd sock picked will definitely match one of previously picked socks. Hence, 3.
+*/
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+/*
+ * Complete the maximumDraws function below.
+ */
+int maximumDraws(int n) {
+    /*
+     * Write your code here.
+     */
+    int x=2*n;
+    return (x-n+1);
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* t_endptr;
+    char* t_str = readline();
+    int t = strtol(t_str, &t_endptr, 10);
+
+    if (t_endptr == t_str || *t_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        char* n_endptr;
+        char* n_str = readline();
+        int n = strtol(n_str, &n_endptr, 10);
+
+        if (n_endptr == n_str || *n_endptr != '\0') { exit(EXIT_FAILURE); }
+
+        int result = maximumDraws(n);
+
+        fprintf(fptr, "%d\n", result);
+    }
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
+
